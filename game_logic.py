8bc74af64c4fcad029a7bf471b1729b91bc07f83
Yiyang Shao <yiyangshao99@gmail.com>
2023-01-26 12:20:05 -0500

    ready for readme

--- game_logic.py
+++ game_logic.py
@@ -8,9 +8,56 @@ class GameLogic:
         self.pan = np.zeros((15,15))
         self.player_turn = 1
         self.playing = True
-        self.ui = ui.UI()
     
     def play_chess(self,pos):
-        if self.pan[pos[0],pos[1]] == 0:
-            self.pan[pos[0],pos[1]] = self.player_turn
-            self.player_turn *= -1
+        if self.playing:
+            if self.pan[pos[0],pos[1]] == 0:
+                self.pan[pos[0],pos[1]] = self.player_turn
+                self.player_turn *= -1
+                return -self.player_turn
+        return None
+
+    def replay(self):
+        self.pan = np.zeros((15,15))
+        self.player_turn = 1
+        self.playing = True
+
+    def win(self):
+        # check rows
+        for i in range(11):
+            for j in range(15):
+                if sum(self.pan[i:i+5,j]) == 5:
+                    self.playing = False
+                    return 1
+                if sum(self.pan[i:i+5,j]) == -5:
+                    self.playing = False
+                    return -1
+        # check columns
+        for i in range(11):
+            for j in range(15):
+                if sum(self.pan[j,i:i+5]) == 5:
+                    self.playing = False
+                    return 1
+                if sum(self.pan[j,i:i+5]) == -5:
+                    self.playing = False
+                    return -1
+        # check diagonals
+        for i in range(11):
+            for j in range(11):
+                if sum(self.pan[i:i+5,j:j+5].diagonal()) == 5:
+                    self.playing = False
+                    return 1
+                if sum(self.pan[i:i+5,j:j+5].diagonal()) == -5:
+                    self.playing = False
+                    return -1
+        # check anti-diagonals
+        for i in range(11):
+            for j in range(11):
+                part = self.pan[i:i+5,j:j+5]
+                if sum(np.fliplr(part).diagonal()) == 5:
+                    self.playing = False
+                    return 1
+                if sum(np.fliplr(part).diagonal()) == -5:
+                    self.playing = False
+                    return -1
+        return 0
--- ui.py
+++ ui.py
@@ -12,35 +12,39 @@ class UI:
         self.resource_repo = repo
         self.event_queue = deque()
         self.running = False
+        self.music_on = 0
+        self.win_cond = 0
+        self.sound_active = 0
         self.width = width
         self.height = height
         self.spacing = 46.42
         self.screen = None
         self.playing = False
         self.gl = gl.GameLogic()
+        self.log = []
 
     def gamestart(self):
         pygame.init()
         pygame.mixer.init(frequency = 8000, channels=1,buffer = 512)
-        bgm = pygame.mixer.Sound("resources/music.wav")
-        sound = pygame.mixer.Sound("resources/sound.wav")
-        im_finishblack = pygame.image.load("resources/finishblack.png")
-        im_finishwhite = pygame.image.load("resources/finishwhite.png")
-        im_pan = pygame.image.load("resources/pan.png")
+        self.bgm = pygame.mixer.Sound("resources/music.wav")
+        self.play_sound = pygame.mixer.Sound("resources/sound.wav")
+        self.im_finishblack = pygame.image.load("resources/finishblack.png")
+        self.im_finishwhite = pygame.image.load("resources/finishwhite.png")
+        self.im_pan = pygame.image.load("resources/pan.png")
         self.im_white = pygame.image.load("resources/white.png")
         self.im_black= pygame.image.load("resources/black.png")
-        im_again = pygame.image.load("resources/again.png")
-        im_musicon = pygame.image.load("resources/music on.png")
-        im_musicoff = pygame.image.load("resources/music off.png")
+        self.im_again = pygame.image.load("resources/again.png")
+        self.im_musicon = pygame.image.load("resources/music on.png")
+        self.im_musicoff = pygame.image.load("resources/music off.png")
         logo = pygame.image.load("resources/gomoku.png")
         pygame.display.set_icon(logo)
         pygame.display.set_caption("minimal program")
         
         screen = pygame.display.set_mode((self.width,self.height))
         self.screen = screen
-        screen.blit(im_pan,(0,0))
-        screen.blit(im_again,(0,self.width))
-        screen.blit(im_musicon,(self.width/2,self.width))
+        screen.blit(self.im_pan,(0,0))
+        screen.blit(self.im_again,(0,self.width))
+        screen.blit(self.im_musicon,(self.width/2,self.width))
         pygame.display.flip()
 
         self.running = True
@@ -59,35 +63,37 @@ class UI:
     def mouse_click(self):
         screen_pos = pygame.mouse.get_pos()
         if screen_pos[1] <= self.width:
-            if playing:
+            if self.playing:
                 x_axis = int((screen_pos[0])//46.42)
                 y_axis = int((screen_pos[1])//46.42)
                 pos = (x_axis,y_axis)
-                gl.play_chess(pos)
-                
-        if pos[1] > 700:
-            if pos[0] <= 350:
+                color = self.gl.play_chess(pos)
+                if color:
+                    self.play_chess(color,pos)
+        if screen_pos[1] > self.width:
+            if screen_pos[0] <= 350:
                 print("play again")
+                self.gl.replay()
                 pan = np.zeros((15,15))
                 color = 1
-                playing = 1
-                screen.blit(im_pan,(0,0))
+                self.playing = 1
+                self.screen.blit(self.im_pan,(0,0))
                 pygame.display.flip()
                 localtime = time.asctime( time.localtime(time.time()) )
                 log = []
             else:
-                if music == 0:
+                if self.music_on == 0:
                     print("music on")
                     pygame.mixer.pre_init(frequency=4000)
-                    pygame.mixer.Channel(1).play(bgm)
-                    screen.blit(im_musicoff,(350,700))
+                    pygame.mixer.Channel(1).play(self.bgm)
+                    self.screen.blit(self.im_musicoff,(350,700))
                     pygame.display.flip()
-                    music = 1
+                    self.music_on = 1
                 else:
-                    music = 0
+                    self.music_on = 0
                     print("music off")
                     pygame.mixer.Channel(1).stop()
-                    screen.blit(im_musicon,(350,700))
+                    self.screen.blit(self.im_musicon,(350,700))
                     pygame.display.flip()
 
     def play_chess(self,color,pos):
@@ -97,3 +103,22 @@ class UI:
         if color == -1:
             self.screen.blit(self.im_white,screen_pos)
         pygame.display.flip()
+        if self.sound_active == 1:
+            pygame.mixer.Channel(2).play(self.sound)
+        self.win_cond = self.gl.win()
+        if self.win_cond == 1:
+            self.win(1)
+        if self.win_cond == -1:
+            self.win(-1)
+
+    def win(self,color):
+        if color == 1: 
+            print("black wins")
+            self.playing = False
+            self.screen.blit(self.im_finishblack,(300,0))
+        if color == -1: 
+            print("white wins")
+            self.playing = False
+            self.screen.blit(self.im_finishwhite,(300,0))
+        pygame.display.flip()
+        
